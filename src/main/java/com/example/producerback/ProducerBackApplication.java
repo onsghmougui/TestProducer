f34690150262b4ef0430cd9da9e93f8805faa6a8
onsghmougui <ons.ghmougui@esprit.tn>
2025-01-27 23:00:50 +0100

    first commit

---
+++ src/main/java/com/example/producerback/ProducerBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.producerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/producerback/ProducerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.producerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
